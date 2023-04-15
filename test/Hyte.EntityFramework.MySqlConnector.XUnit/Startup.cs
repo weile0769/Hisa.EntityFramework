c7547c5885025c2bb714a5cd4a081856d6d06930
L.WLe <184483622@qq.com>
2023-04-16 01:12:05 +0800

    🧪 test: 添加MySqlConnector实体框架实现测试案例

---
+++ test/Hyte.EntityFramework.MySqlConnector.XUnit/Startup.cs
@@ -0,0 +1,23 @@
+using Hyte.EntityFramework.Options;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hyte.EntityFramework.MySqlConnector.XUnit;
+
+/// <summary>
+///     项目启动配置类
+/// </summary>
+public class Startup
+{
+    /// <summary>
+    ///     配置服务
+    /// </summary>
+    /// <param name="services">容器服务</param>
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //注册数据库实体框架
+        services.AddHyteEntityFramework(options =>
+        {
+            options.UseMySqlConnector(connectOptions => { connectOptions.ConnectionString = ""; });
+        });
+    }
+}
