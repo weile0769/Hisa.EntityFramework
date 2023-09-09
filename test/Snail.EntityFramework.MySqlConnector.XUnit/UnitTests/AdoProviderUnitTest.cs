67541ac2c241899ee3c1e9e6cfcb65d268bae9ec
L.WLe <184483622@qq.com>
2023-09-09 19:00:26 +0800

    🧪 test: 添加SQL对象参数化查询测试用例

--- test/Snail.EntityFramework.MySqlConnector.XUnit/UnitTests/AdoProviderUnitTest.cs
+++ test/Snail.EntityFramework.MySqlConnector.XUnit/UnitTests/AdoProviderUnitTest.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using Snail.EntityFramework.Models;
 using Snail.EntityFramework.MySqlConnector.XUnit.Entities;
 
 namespace Snail.EntityFramework.MySqlConnector.XUnit.UnitTests;
@@ -22,18 +24,59 @@ public class AdoProviderUnitTest
     }
 
     /// <summary>
-    ///     SQL查询单元测试案例
+    ///     SQL非参数化查询单元测试案例
     /// </summary>
-    [Fact(DisplayName = "SQL查询单元测试案例")]
-    public void SqlQueryUnitTest()
+    [Fact(DisplayName = "SQL非参数化查询单元测试案例")]
+    public void SqlQueryNoSqlParameterUnitTest()
     {
         var sql = @"
 select id          as Id,
        create_time as CreateTime,
        modify_time as ModifyTime
-from user
+from user where id>1
 ";
         var list = _adoProvider.SqlQuery<User>(sql);
+        Assert.NotEmpty(list);
+    }
+
+    /// <summary>
+    ///     SQL参数化查询单元测试案例
+    /// </summary>
+    [Fact(DisplayName = "SQL参数化查询单元测试案例")]
+    public void SqlQueryIncludeSqlParameterUnitTest()
+    {
+        var sql = @"
+select id          as Id,
+       create_time as CreateTime,
+       modify_time as ModifyTime
+from user where id>@id
+";
+        var list = _adoProvider.SqlQuery<User>(sql, new SqlParameter
+        {
+            DbType = DbType.Int64,
+            ParameterName = "id",
+            Value = 1
+        });
+        Assert.NotEmpty(list);
+    }
+
+    /// <summary>
+    ///     SQL对象参数化查询单元测试案例
+    /// </summary>
+    [Fact(DisplayName = "SQL对象参数化查询单元测试案例")]
+    public void SqlQueryIncludeObjectParameterUnitTest()
+    {
+        var sql = @"
+select id          as Id,
+       create_time as CreateTime,
+       modify_time as ModifyTime
+from user where id>@id and create_time<@createTime
+";
+        var list = _adoProvider.SqlQuery<User>(sql, new
+        {
+            id = 1,
+            createTime = DateTime.Now
+        });
         Assert.NotEmpty(list);
     }
 }
