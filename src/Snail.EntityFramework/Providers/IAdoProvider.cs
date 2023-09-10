34ae440771e917088e4ee20e94aaf8f3677ac22b
L.WLe <184483622@qq.com>
2023-09-10 17:45:09 +0800

    ✨ feat: 添加SqlQuerySingle单个实体SQL查询函数

--- src/Snail.EntityFramework/Providers/IAdoProvider.cs
+++ src/Snail.EntityFramework/Providers/IAdoProvider.cs
@@ -8,6 +8,8 @@ namespace Snail.EntityFramework;
 /// </summary>
 public interface IAdoProvider
 {
+    #region GetDataReader
+
     /// <summary>
     ///     获取数据读取器对象
     /// </summary>
@@ -16,6 +18,32 @@ public interface IAdoProvider
     /// <param name="parameters">参数</param>
     /// <returns></returns>
     IDataReader GetDataReader(IDbConnection connection, string sql, params SqlParameter[] parameters);
+
+    #endregion
+
+    #region SqlQuerySingle
+
+    /// <summary>
+    ///     SQL查询
+    /// </summary>
+    /// <param name="sql">SQL脚本</param>
+    /// <param name="parameter">查询参数</param>
+    /// <typeparam name="T">查询结果对象类型</typeparam>
+    /// <returns>查询结果实体对象</returns>
+    T SqlQuerySingle<T>(string sql, object parameter);
+
+    /// <summary>
+    ///     SQL查询
+    /// </summary>
+    /// <param name="sql">SQL脚本</param>
+    /// <param name="parameters">查询参数</param>
+    /// <typeparam name="T">查询结果对象类型</typeparam>
+    /// <returns>查询结果实体对象</returns>
+    T SqlQuerySingle<T>(string sql, params SqlParameter[] parameters);
+
+    #endregion
+
+    #region SqlQuery
 
     /// <summary>
     ///     SQL查询
@@ -34,4 +62,6 @@ public interface IAdoProvider
     /// <typeparam name="T">查询结果对象类型</typeparam>
     /// <returns>查询结果实体对象列表</returns>
     List<T> SqlQuery<T>(string sql, params SqlParameter[] parameters);
+
+    #endregion
 }
